bc8071bbee392c7390f6a7f67fa2f7f706cd1ead
Steve Holsinger <steve@mokucollective.com>
2015-12-17 01:23:21 -0500

    Initial commit. Lots of node-init boiler plate still evident.

---
+++ README.md
@@ -0,0 +1,27 @@
+# ThreePointEstimation
+
+Assists in calculating the three-point estimate using the mean and standard deviation of the best, worst, and realistic estimate values.
+
+## Getting Started
+Install the module with: `npm install ThreePointEstimation`
+
+```javascript
+var ThreePointEstimation = require('ThreePointEstimation');
+ThreePointEstimation.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2015 Steve Holsinger  
+Licensed under the MIT license.
---
+++ test/ThreePointEstimation_test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var ThreePointEstimation = require('../lib/ThreePointEstimation.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['main'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(2);
+    // tests here
+    test.equal(ThreePointEstimation.createEstimate(10, 20, 15), 18.333333333333332, 'should be 18.333333333333332');
+    test.throws(ThreePointEstimation.createEstimate());
+    test.done();
+  },
+};
